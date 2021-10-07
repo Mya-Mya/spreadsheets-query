c78022156eefb28c10b466655a51135943896906
Mya-Mya <interleukin413@gmail.com>
2021-10-07 21:42:13 +0900

    Add method : checks whether the text is valid for sheetname

--- src/querylang07/security.js
+++ src/querylang07/security.js
@@ -6,3 +6,12 @@ const STRING_COMPARING_CLAUSE_INJECTOR = /["'`]/g;
  */
 export const removeStringComparingClauseInjector = (text) =>
   text.replace(STRING_COMPARING_CLAUSE_INJECTOR, "");
+
+const INVALID_SHEETNAME_PATTERN = /["'`]/;
+
+/**
+ *
+ * @param {String} text
+ * @returns {Boolean}
+ */
+export const isValidSheetname = (text) => !INVALID_SHEETNAME_PATTERN.test(text);
