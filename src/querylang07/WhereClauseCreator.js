b3f2a57135539868165e58f65e74900031fe115c
Mya-Mya <interleukin413@gmail.com>
2021-10-07 21:24:04 +0900

    Rename : fieldValue -> value

--- src/querylang07/WhereClauseCreator.js
+++ src/querylang07/WhereClauseCreator.js
@@ -68,55 +68,55 @@ class WhereClauseCreator {
    */
   _getComparingClause(comparing) {
     const c = this._mapColumnNameToColumnId[comparing.getColumnName()];
-    const f = comparing.getValue();
+    const v = comparing.getValue();
     switch (comparing.getComparingType()) {
       //String
       case ComparingTypes.STRING_EQUALS:
-        return this._getStringComparingClause(c, f, "=");
+        return this._getStringComparingClause(c, v, "=");
       case ComparingTypes.STRING_STARTS_WITH:
-        return this._getStringComparingClause(c, f, "STARTS WITH");
+        return this._getStringComparingClause(c, v, "STARTS WITH");
       case ComparingTypes.STRING_ENDS_WITH:
-        return this._getStringComparingClause(c, f, "ENDS WITH");
+        return this._getStringComparingClause(c, v, "ENDS WITH");
       case ComparingTypes.STRING_CONTAINS:
-        return this._getStringComparingClause(c, f, "CONTAINS");
+        return this._getStringComparingClause(c, v, "CONTAINS");
       //StringNot
       case ComparingTypes.STRING_NOT_EQUALS:
-        return this._getStringComparingClause(c, f, "!=");
+        return this._getStringComparingClause(c, v, "!=");
       case ComparingTypes.STRING_NOT_STARTS_WITH:
-        return this._not(this._getStringComparingClause(c, f, "STARTS WITH"));
+        return this._not(this._getStringComparingClause(c, v, "STARTS WITH"));
       case ComparingTypes.STRING_NOT_ENDS_WITH:
-        return this._not(this._getStringComparingClause(c, f, "ENDS WITH"));
+        return this._not(this._getStringComparingClause(c, v, "ENDS WITH"));
       case ComparingTypes.STRING_NOT_CONTAINS:
-        return this._not(this._getStringComparingClause(c, f, "CONTAINS"));
+        return this._not(this._getStringComparingClause(c, v, "CONTAINS"));
       //Number
       case ComparingTypes.NUMBER_EQUALS:
-        return this._getNumberComparingClause(c, f, "=");
+        return this._getNumberComparingClause(c, v, "=");
       case ComparingTypes.NUMBER_LAGER_THAN:
-        return this._getNumberComparingClause(c, f, ">");
+        return this._getNumberComparingClause(c, v, ">");
       case ComparingTypes.NUMBER_LAGER_OR_EQUALS:
-        return this._getNumberComparingClause(c, f, ">=");
+        return this._getNumberComparingClause(c, v, ">=");
       case ComparingTypes.NUMBER_LESS_THAN:
-        return this._getNumberComparingClause(c, f, "<");
+        return this._getNumberComparingClause(c, v, "<");
       case ComparingTypes.NUMBER_LESS_OR_EQUALS:
-        return this._getNumberComparingClause(c, f, "<=");
+        return this._getNumberComparingClause(c, v, "<=");
       //NumberNot
       case ComparingTypes.NUMBER_NOT_EQUALS:
-        return this._getNumberComparingClause(c, f, "!=");
+        return this._getNumberComparingClause(c, v, "!=");
       //Boolean
       case ComparingTypes.BOOLEAN:
-        return this._getBooleanComparingClause(c, f);
+        return this._getBooleanComparingClause(c, v);
     }
   }
 
   /**
    *
    * @param {String} columnId
-   * @param {any} fieldValue
+   * @param {any} value
    * @param {String} operator
    */
-  _getStringComparingClause(columnId, fieldValue, operator) {
-    const fieldValueString = this._removeInjectingString(fieldValue);
-    return `${columnId} ${operator} '${fieldValueString}'`;
+  _getStringComparingClause(columnId, value, operator) {
+    const valueString = this._removeInjectingString(value);
+    return `${columnId} ${operator} '${valueString}'`;
   }
 
   /**
@@ -131,18 +131,18 @@ class WhereClauseCreator {
   /**
    *
    * @param {String} columnId
-   * @param {any} fieldValue
+   * @param {any} value
    * @param {String} operator
    */
-  _getNumberComparingClause(columnId, fieldValue, operator) {
-    const fieldValueNumber = Number(fieldValue);
-    return `${columnId} ${operator} ${fieldValueNumber.toString()}`;
+  _getNumberComparingClause(columnId, value, operator) {
+    const valueNumber = Number(value);
+    return `${columnId} ${operator} ${valueNumber.toString()}`;
   }
 
-  _getBooleanComparingClause(columnId, fieldValue) {
-    const fieldValueBoolean = Boolean(fieldValue);
-    const fieldValueBooleanExpr = fieldValueBoolean ? "true" : "false";
-    return `${columnId} = ${fieldValueBooleanExpr}`;
+  _getBooleanComparingClause(columnId, value) {
+    const valueBoolean = Boolean(value);
+    const valueBooleanExpr = valueBoolean ? "true" : "false";
+    return `${columnId} = ${valueBooleanExpr}`;
   }
   /**
    *
