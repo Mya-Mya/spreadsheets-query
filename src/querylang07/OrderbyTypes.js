4fc49c012676b20990bdbaa339df9f84fca47c5e
Mya-Mya <interleukin413@gmail.com>
2021-10-08 23:52:12 +0900

    Create OrderbyTypes.js

---
+++ src/querylang07/OrderbyTypes.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef {String} OrderbyType
+ */
+
+/**
+ * @readonly
+ * @enum {OrderbyType} */
+const OrderbyTypes = {
+  ASC: "Ascending",
+  DESC: "Descending",
+};
+export default OrderbyTypes;
