74f973732c7133ba65e9ab092d4442db157c7252
Mya-Mya <interleukin413@gmail.com>
2021-10-09 14:12:20 +0900

    Create createOrGetSheet

--- src/spreadsheetUtils.js
+++ src/spreadsheetUtils.js
@@ -1,9 +1,25 @@
+//@ts-check
 /**
  * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet
+ * @returns {any[]}
  */
 export const getHeaderValues = (sheet) => {
   const lastColumn = sheet.getLastColumn();
   const range = sheet.getRange(1, 1, 1, lastColumn);
   const values = range.getValues()[0];
   return values;
+};
+
+/**
+ * @param {GoogleAppsScript.Spreadsheet.Spreadsheet} spreadsheet
+ * @param {String} sheetName
+ * @returns {GoogleAppsScript.Spreadsheet.Sheet}
+ */
+export const createOrGetSheet = (spreadsheet, sheetName) => {
+  const sheets = spreadsheet.getSheets();
+  if (sheets.findIndex((i) => i.getSheetName() === sheetName) == -1) {
+    spreadsheet.insertSheet(sheetName);
+  }
+  const sheet = spreadsheet.getSheetByName(sheetName);
+  return sheet;
 };
