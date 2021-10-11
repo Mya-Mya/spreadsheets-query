b178ac0d1975015d804c48494c3b55258e2111aa
Mya-Mya <interleukin413@gmail.com>
2021-10-11 10:19:50 +0900

    Create method returns all values in sheet

--- src/linearsearcher/LinearSearcher.js
+++ src/linearsearcher/LinearSearcher.js
@@ -15,11 +15,17 @@ class LinearSearcher {
    */
   selectIf(predicate) {
     const headerValues = getHeaderValues(this._sheet);
-    const values = this._sheet
-      .getRange(2, 1, this._sheet.getLastRow() - 1, this._sheet.getLastColumn())
-      .getValues()
-      .filter(predicate);
+    const values = this._getAllValuesFromSeet().filter(predicate);
     return new SelectResult(headerValues, values);
+  }
+  /**
+   *
+   * @returns {any[][]}
+   */
+  _getAllValuesFromSeet() {
+    return this._sheet
+      .getRange(2, 1, this._sheet.getLastRow() - 1, this._sheet.getLastColumn())
+      .getValues();
   }
 }
 export default LinearSearcher;
